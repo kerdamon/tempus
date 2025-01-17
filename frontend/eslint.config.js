ae0470c0b636bc7b8a3622c2e5a9465a02575dba
kerdamon <kwalas314@gmail.com>
2025-01-17 19:09:51 +0100

    chore(frontend): add tanstack query

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,15 +1,16 @@
 import js from '@eslint/js';
-import prettier from 'eslint-plugin-prettier';
-import react from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
+import queryPlugin from '@tanstack/eslint-plugin-query';
+import prettierPlugin from 'eslint-plugin-prettier';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import reactRefreshPlugin from 'eslint-plugin-react-refresh';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist', 'src/components/ui'] },
   {
-    settings: { react: { version: '^18.3.1' } },
+    settings: { reactPlugin: { version: '^18.3.1' } },
     extends: [
       js.configs.recommended,
       ...tseslint.configs.strictTypeChecked,
@@ -25,20 +26,22 @@ export default tseslint.config(
       },
     },
     plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      prettier,
+      reactPlugin,
+      'react-hooks': reactHooksPlugin,
+      'react-refresh': reactRefreshPlugin,
+      queryPlugin,
+      prettierPlugin,
     },
     rules: {
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...reactPlugin.configs['jsx-runtime'].rules,
+      ...reactHooksPlugin.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
-      ...prettier.configs.recommended.rules,
+      ...queryPlugin.configs.recommended.rules,
+      ...prettierPlugin.configs.recommended.rules,
     },
   }
 );
